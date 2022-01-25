21c37a5d0cfcb7544ca327ccc2592eb9fb7a8b4b
guilhermeoliveira94 <guilhermeoliveira.94@hotmail.com>
2022-01-25 12:00:56 -0300

    100 Days of Code Python

--- Day 8/8.2 Prime Number Checker/main.py
+++ Day 8/8.2 Prime Number Checker/main.py
@@ -1,5 +1,23 @@
 # Write your code below this line 👇
+def prime_checker(number):
+    # define a flag variable
+    flag = False
 
+    # prime numbers are greater than 1
+    if number > 1:
+        # check for factors
+        for i in range(2, number):
+            if (number % i) == 0:
+                # if factor is found, set flag to True
+                flag = True
+                # break out of loop
+                break
+
+    # check if flag is True
+    if flag:
+        print(number, "is not a prime number.")
+    else:
+        print(number, "is a prime number.")
 
 # Write your code above this line 👆
 
