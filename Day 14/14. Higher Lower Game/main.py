a221dfc2cf7616b292959c8bc33dd77de7e46df6
guilhermeoliveira94 <guilhermeoliveira.94@hotmail.com>
2022-01-28 14:31:34 -0300

    100 Days of Code Python

---
+++ Day 14/14. Higher Lower Game/main.py
@@ -0,0 +1,46 @@
+from art import logo, vs
+from game_data import data
+import random
+
+score = 0
+
+def game():
+    global score
+
+    print(logo)
+
+    sorted_name_a = []
+    sorted_name_a.append(data[random.randint(0, len(data) - 1)])
+
+    sorted_name_b = []
+    sorted_name_b.append(data[random.randint(0, len(data) - 1)])
+
+    print(f"Compare A: {sorted_name_a[0]['name']}, a {sorted_name_a[0]['description']}, from {sorted_name_a[0]['country']}.")
+    print(vs)
+    print(f"Compare B: {sorted_name_b[0]['name']}, a {sorted_name_b[0]['description']}, from {sorted_name_b[0]['country']}.")
+
+    guess = input("Who has more followers? Type 'A' or 'B': ").lower()
+    if guess == "a":
+        guess = sorted_name_a[0]['follower_count']
+        if guess > sorted_name_b[0]['follower_count']:
+            score += 1
+            print(f"You're right! Current score: {score}. ")
+            game()
+
+        else:
+            print(f"Sorry, that's wrong. Final score: {score}")
+            exit()
+    elif guess == "b":
+        guess = sorted_name_b[0]['follower_count']
+        if guess > sorted_name_a[0]['follower_count']:
+            score += 1
+            print(f"You're right! Current score: {score}. ")
+            game()
+
+        else:
+            print(f"Sorry, that's wrong. Final score: {score}")
+            exit()
+    else:
+        print("Wrong input.")
+
+game()
