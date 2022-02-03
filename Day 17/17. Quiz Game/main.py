bc814dff5b73285f0243a6d19c906dfa1302a658
guilhermeoliveira94 <guilhermeoliveira.94@hotmail.com>
2022-02-03 14:19:51 -0300

    100 Days of Code Python

--- Day 17/17. Quiz Game/main.py
+++ Day 17/17. Quiz Game/main.py
@@ -1,23 +1,51 @@
-class User:
+from question_model import Question
+from data import question_data
+from quiz_brain import Quizbrain
 
-        def __init__(self, user_id, username):
-            self.id = user_id
-            self.username = username
-            self.followers = 0
-            self.following = 0
+question_bank = []
 
-        def follow(self, user):
-            user.followers += 1
-            user.following += 1
+for question in question_data:
+    question_text = question["text"]
+    question_answer = question["answer"]
+    new_question = Question(question_text, question_answer)
+    question_bank.append(new_question)
+
+quiz = Quizbrain(question_bank)
+
+while quiz.still_has_questions():  # if quiz still has questions remaining:
+    quiz.nexquestion()
 
 
-user_1 = User("001", "angela")
-user_2 = User("002", "jack")
 
-user_1.follow(user_2)
 
-print(user_1.followers)
-print(user_1.following)
 
-print(user_2.followers)
-print(user_2.following)
+
+
+
+
+
+
+
+# class User:
+#
+#         def __init__(self, user_id, username):
+#             self.id = user_id
+#             self.username = username
+#             self.followers = 0
+#             self.following = 0
+#
+#         def follow(self, user):
+#             user.followers += 1
+#             user.following += 1
+#
+#
+# user_1 = User("001", "angela")
+# user_2 = User("002", "jack")
+#
+# user_1.follow(user_2)
+#
+# print(user_1.followers)
+# print(user_1.following)
+#
+# print(user_2.followers)
+# print(user_2.following)
--- Day 17/17. Quiz Game/question_model.py
+++ Day 17/17. Quiz Game/question_model.py
@@ -4,8 +4,3 @@ class Question:
         self.text = text
         self.answer = answer
 
-
-question1 = Question("Onde surgiu o grupo Eco Road?", "Guareí Velho")
-
-print(question1.text)
-print(question1.answer)
